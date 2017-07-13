0b76cf8d904538d2496b47839c918c984f743234
Rogelio Ventura <rogeliovr94@gmail.com>
2017-07-13 07:02:26 -0500

    Modificación para muestreo de fecha y hora en json

--- PHP/add_data.php
+++ PHP/add_data.php
@@ -7,12 +7,13 @@
 	  $presion_dis=$_POST["presion_dis"];
 	  $presion_sis=$_POST["presion_sis"];
 	  $pulso=$_POST["pulso"];
+    $fecha=date("l d-m-Y h:i:s a");
 	  $peticion="INSERT INTO paciente VALUES (NULL, NULL, $temperatura, $presion_sis, $presion_dis, $pulso)";
 
     $link->query($peticion);
 
    	$link->close();
-
+    $send_data -> fecha = $fecha;
     $send_data -> temperatura = $temperatura;
     $send_data -> presion_distolica = $presion_dis;
     $send_data -> presion_sistolica = $presion_sis;
