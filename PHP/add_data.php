086e3acde545d6da5062c5bef16c22021de20cfd
Rogelio Ventura <rogeliovr94@gmail.com>
2017-08-20 19:29:48 -0500

    Pruebad e json

--- PHP/add_data.php
+++ PHP/add_data.php
@@ -2,7 +2,7 @@
    	include "../../../connect_e_health.php";
    	
    	$link=db_Connection();
-    $fecha=date("l d-m-Y h:i:s a");
+
 	  $temperatura=$_POST["temperatura"];
     $pulso=$_POST["pulso"];
     $contracciones=$_POST["contracciones"];
@@ -11,10 +11,6 @@
     $frecuencia_fetal=$_POST["frecuencia_fetal"];
 	  $rand=mt_rand(1, 3);
     echo $rand;
-    $json_string = file_get_contents('data.json')
-    $datos=json_decode($json_string, true);
-    echo $temperatura;
-    //$send_data -> fecha = $fecha;
     switch ($rand){
       case 1:
         $presion_sis=115;
@@ -31,45 +27,51 @@
     }	  
     if ($temperatura){
       echo "temperatura \n";
-      $datos[0]['fecha']=$fecha;
-      $datos[0]['temperatura']=$temperatura;
-      //$send_data -> temperatura = $temperatura;
+
     }
     elseif ($pulso){
       echo "pulso \n";
-      //$send_data -> pulso = $pulso;
+      
     }
     elseif ($glucosa){
       echo "glucosa\n";
-      //$send_data -> glucosa = $glucosa;
+      
     }
     elseif ($frecuencia_respiratoria){
       echo "frecuencia respiartoria\n";
-      //$send_data -> frecuencia_respiratoria = $frecuencia_respiratoria;
+      
     }
     elseif ($contracciones){
       echo "contracciones\n";
-      //$send_data -> contracciones = $contracciones;
+      
     }
     elseif ($frecuencia_fetal){
       echo "frecuencia fetal\n";
-      //$send_data -> frecuencia_fetal = $frecuencia_fetal;
+      
     }
     elseif (!$temperatura || !$pulso || !$glucosa || !$frecuencia_respiratoria || !$frecuencia_fetal || !$contracciones){
       echo "presion \n";
-      //$send_data -> presion_distolica = $presion_dis;
-      //$send_data -> presion_sistolica = $presion_sis;
+      
     }
     
-    
+    $fecha=date("l d-m-Y h:i:s a");
 	  $peticion="INSERT INTO pacientes VALUES (NULL, NULL, $temperatura, $presion_sis, $presion_dis, $pulso, $contracciones, $frecuencia_respiratoria, $glucosa, $frecuencia_fetal)";
 
     $link->query($peticion);
 
    	$link->close();
+    $send_data -> fecha = $fecha;
+    $send_data -> temperatura = $temperatura;
+    $send_data -> presion_distolica = $presion_dis;
+    $send_data -> presion_sistolica = $presion_sis;
+    $send_data -> pulso = $pulso;
+    $send_data -> contracciones = $contracciones;
+    $send_data -> frecuencia_respiratoria = $frecuencia_respiratoria;
+    $send_data -> glucosa = $glucosa;
+    $send_data -> frecuencia_fetal = $frecuencia_fetal;
 
-    //$myJSON = json_encode($send_data);
-    $myJSON = json_encode($datos);
+    $myJSON = json_encode($send_data);
+
     $archivo = fopen("data.json", "w");
     fwrite($archivo, "[");
     fwrite($archivo, $myJSON);
